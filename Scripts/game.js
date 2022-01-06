ff9cbd4977986bdb5e38015b986c2025c789b1c9
WillianDomingues79 <willian.souza79@hotmail.com>
2022-01-06 13:19:28 -0300

    Adicionado figuras e função de clicar e aparecer a figura da vez

---
+++ Scripts/game.js
@@ -0,0 +1,17 @@
+//Iniciar váriaveis
+let board = ['','','','','','','','',''];
+let playerTime = 0;
+let symbols = ['O', 'X'];
+
+function handleMove(position){
+
+    if (board[position] == ''){
+    board[position] = symbols[playerTime];
+
+    if (playerTime == 0){
+        playerTime = 1;
+    } else {
+        playerTime = 0;
+    }
+}
+}
