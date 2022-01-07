443ac3ee3b97adae7e3cb69c16a965ff79686beb
WillianDomingues79 <willian.souza79@hotmail.com>
2022-01-06 21:35:48 -0300

    Adicionado função de jogo e logica

--- Scripts/interface.js
+++ Scripts/interface.js
@@ -1,31 +1,53 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square) =>{
+    squares.forEach((square) => {
         square.addEventListener("click", handleClick);
     })
 })
 
-function handleClick(event){
+function handleClick(event) {
     console.log(event.target);
 
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+            if (playerTime == 1){
+                playerTime = "Cruz"
+            }else{
+                playerTime = "Bolinha"
+            }
+
+            if (handleMove = false){
+                alert ("Deu empate");
+            }else{
+                alert ("O jogo acabou - O vencedor foi o jogador "+ playerTime);
+            }
+            
+        }, 10);
+    };
+    updateSquare(position);
 }
 
-function updateSquares(){
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`;
+}
+
+/*function updateSquares() {
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square) =>{
+    squares.forEach((square) => {
         let position = square.id;
         let symbol = board[position];
 
-        if (symbol != ''){
+        if (symbol != '') {
             square.innerHTML = `<div class='${symbol}'></div>`;
         }
     })
-}
+}*/
