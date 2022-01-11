f7811726da35eb4440c46edba9d6a2090af4d2ab
WillianDomingues79 <willian.souza79@hotmail.com>
2022-01-10 22:21:49 -0300

    Adicionado função de empate

--- Scripts/game.js
+++ Scripts/game.js
@@ -4,26 +4,38 @@ let playerTime = 0;
 let symbols = ['O', 'X'];
 let gameOver = false;
 
+let countTurn0 = 0;//Cria um contador para passar nas vezes que troca a vez para quando algum dos 2 chegar a 5 empatar o jogo
+let countTurn1 = 0;
+
 function handleMove(position) {
-
-    if (gameOver) {
+    
+    /*if (gameOver) {
         return;
-    }
-
+    }*/
+    
     if (board[position] == '') {
         board[position] = symbols[playerTime];
 
         gameOver = isWin();
-
+        
+        
         if (gameOver == false) {
-            if (playerTime == 0) {
+            if (playerTime == 0) {//Força o playertime fica no 0 e 1 para pegar os simbolos certos
+                countTurn0++;
+                console.log("0"+countTurn0)
                 playerTime = 1;
             } else {
+                countTurn1++;
+                console.log("1"+countTurn1)
                 playerTime = 0;
             }
+
+                                    
         }
+
     }
-    return gameOver;
+
+    return gameOver;    
 }
 
 function isWin() {
@@ -38,20 +50,25 @@ function isWin() {
         [2, 4, 6]
     ]
 
+    
     for (let i = 0; i < winStates.length; i++) {
         let seq = winStates[i];
-
+        
         let pos1 = seq[0];
         let pos2 = seq[1];
         let pos3 = seq[2];
-
+        
         if (board[pos1] == board[pos2] &&
             board[pos1] == board[pos3] &&
             board[pos1] != '') {
             return true;
         }
-        
+
     }
+
     return false;
+     
 }
+
+
 
--- Scripts/interface.js
+++ Scripts/interface.js
@@ -4,41 +4,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
     squares.forEach((square) => {
         square.addEventListener("click", handleClick);
+         
     })
+    
 })
 
 function handleClick(event) {
-    console.log(event.target);
-
+    
     let square = event.target;
     let position = square.id;
 
     if (handleMove(position)) {
 
         setTimeout(() => {
-            if (playerTime == 1){
+            
+            if (playerTime == 1) {
                 playerTime = "Cruz"
-            }else{
+            } else {
                 playerTime = "Bolinha"
             }
-
-            if (handleMove = false){
-                alert ("Deu empate");
-            }else{
-                alert ("O jogo acabou - O vencedor foi o jogador "+ playerTime);
-            }
-            
+            alert("O jogo acabou - O vencedor foi o jogador " + playerTime);
         }, 10);
+        
     };
+    
+    if ((countTurn0 >= 5 || countTurn1 >= 5) && !gameOver) {
+        setTimeout(() => {
+          alert("Deu empate!\nClique no botão abaixo e tente novamente.");
+        }, 20);
+      }
+
     updateSquare(position);
+    
 }
 
-function updateSquare(position){
+function updateSquare(position) {
+        
     let square = document.getElementById(position.toString());
     let symbol = board[position];
     square.innerHTML = `<div class='${symbol}'></div>`;
+    
 }
-
+    
 /*function updateSquares() {
     let squares = document.querySelectorAll(".square");
 
